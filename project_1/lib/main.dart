c447ae7fa897b5332d7d3504ce1f43180bbcce45
TheTran24 <133380265+TheTran24@users.noreply.github.com>
2023-08-10 09:36:40 +0700

    dice

--- project_1/lib/main.dart
+++ project_1/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:project_1/newfile.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: (Scaffold(body: GradientContainer(Color.fromARGB(255, 248, 246, 246), Color.fromARGB(255, 8, 8, 8)))),
+      home: (Scaffold(body: GradientContainer.supercolors())),
     ),
   );
 }
--- project_1/lib/newfile.dart
+++ project_1/lib/newfile.dart
@@ -1,29 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:project_1/texting.dart';
 
 const startline = Alignment.topLeft;
 const endline = Alignment.bottomLeft;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this. color1, this.color2, {super.key} );
+  const GradientContainer(this.color1, this.color2, {super.key});
 
-
+const GradientContainer. supercolors({super.key}) :
+ color1 =const Color.fromARGB(209, 187, 55, 169),
+ color2= const Color.fromARGB(225, 0, 226, 121);
   final Color color1;
-  final  Color color2 ;
-
+  final Color color2;
+ 
 
   @override
   Widget build(BuildContext context) {
-    return Container (
+    return Container(
       decoration: BoxDecoration(
-        gradient:  LinearGradient(
-        colors:[ color1 , color2 ],
-            
-            begin: startline,
-            end: endline),
+        gradient: LinearGradient(
+            colors: [color1, color2], begin: startline, end: endline),
       ),
-      child: const Center(child: texting('Justkiding')),
-    )
-    ;
+      child: Center(
+        child: Image.asset('Picture/1.png', width: 200,),
+      ),
+    );
   }
 }
